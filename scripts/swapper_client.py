f20af5f9b800d2ff9c6ebb98c14fcc7b66976dad
Raul Peralta-Lozada <raulpl25@gmail.com>
2016-01-22 14:48:58 -0600

    Adding a verification between initialpose and amcl_pose

--- scripts/swapper_client.py
+++ scripts/swapper_client.py
@@ -23,40 +23,31 @@ if __name__ == '__main__':
     try:
         # service_name = str(sys.argv[1])
         # print(service_call(service_name))
-        rospy.init_node('swapper_client_node', anonymous=True)
+        rospy.init_node('swapper_client_node')
         dist = float(sys.argv[1])
         pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped,
-                              queue_size=1, latch=True)
-        # pose = PoseWithCovarianceStamped()
-        # pose.pose.pose.position.x = dist
-        # pose.pose.pose.orientation.w = 1.0
-        # cov = []
-        # for i in range(6):
-        #     for j in range(6):
-        #         if i == j:
-        #             cov.append(0.25)
-        #         else:
-        #             cov.append(0.0)
-        # pose.pose.covariance = cov
-        p = PoseWithCovarianceStamped()
-        msg = PoseWithCovariance()
-        msg.pose = Pose(Point(-0.767, -0.953, 0.000),
-                        Quaternion(0.000, 0.000, -0.0, 0.9999))
-        msg.covariance = [0.25, 0.0, 0.0, 0.0, 0.0, 0.0,
-                          0.0, 0.25, 0.0, 0.0, 0.0, 0.0,
-                          0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                          0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                          0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-                          0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942]
-        p.pose = msg
-        p.header.frame_id = 'map'
-        p.header.stamp = rospy.Time.now()
-        print(p)
-        pub.publish(p)
-        # pose.header.stamp = rospy.Time.now()
-        # pose.header.frame_id = 'map'
-        # print(pose)
-        # pub.publish(pose)
+                              latch=True)
+        pose_msg = PoseWithCovarianceStamped()
+        pose_msg.pose.pose.position = Point(-1.0, -1.0, 0.000)
+        pose_msg.pose.pose.orientation = Quaternion(0.0, 0.0, 0.0, 1.0)
+        covariance = [0.2, 0.0, 0.0, 0.0, 0.0, 0.0,
+                      0.0, 0.2, 0.0, 0.0, 0.0, 0.0,
+                      0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                      0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                      0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                      0.0, 0.0, 0.0, 0.0, 0.0, 0.06]
+        pose_msg.pose.covariance = covariance
+        pose_msg.header.frame_id = 'map'
+        pose_msg.header.stamp = rospy.Time.now()
+        print(pose_msg)
+        while True:
+            pose_msg.header.stamp = rospy.Time.now()
+            pub.publish(pose_msg)
+            c_pose = rospy.wait_for_message('/amcl_pose',
+                                            PoseWithCovarianceStamped)
+            if pose_msg.pose.pose.position.x == c_pose.pose.pose.position.x:
+                if pose_msg.pose.pose.position.y == c_pose.pose.pose.position.y:
+                    break
 
     except rospy.ROSInterruptException:
         print('Exception')
